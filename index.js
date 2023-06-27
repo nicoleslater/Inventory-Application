4f46e9bda6f10f16de57021ba94615a0d1387377
Nicole Slater <nicoleslater@pursuit.org>
2023-06-26 21:33:45 -0400

    Making Magic

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const title = document.getElementById('title')
-
+const author = document.getElementById('author')
+const price = document.getElementById('price')
+const inStock = document.getElementById('InStock')
