fe3b39c76f997dc358d411eb727ecb6c70944d0e
Nicole Slater <nicoleslater@pursuit.org>
2023-06-26 21:36:18 -0400

    My Favorite Project

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const title = document.getElementById('title')
 const author = document.getElementById('author')
 const price = document.getElementById('price')
 const inStock = document.getElementById('InStock')
+const bookList = document.getElementById('book-list')
+
