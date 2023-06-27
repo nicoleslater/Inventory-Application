f8001b4035a078101238f9910a38731206fa4f4a
Nicole Slater <nicoleslater@pursuit.org>
2023-06-26 22:15:00 -0400

    Pushing through

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const author = document.getElementById('author')
 const price = document.getElementById('price')
 const inStock = document.getElementById('InStock')
 const bookList = document.getElementById('book-list')
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', function(e){
+    e.preventDefault()
+
+    if(title.value == "" && author.value == "" && price.value == ""){
+        alert("Fill The Form");
+    }
+
+})
+
+console.log(title)
+console.log(author)
+console.log(price)
+console.log(inStock)
+console.log(bookList)
+console.log(btn)
+
+
 
 
 
